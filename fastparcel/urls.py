979bc24bb653f538c0794af0afe6d7ad800d4d60
Your Name <you@example.com>
2021-10-02 16:59:34 +0530

    added signup url

--- fastparcel/urls.py
+++ fastparcel/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 
     path('sign-in/', auth_views.LoginView.as_view(template_name="sign_in.html")),
     path('sign-out/', auth_views.LogoutView.as_view(next_page="/")),
+    path('sign-up/', views.sign_up, name="Signuppage"),
 
     path('customer/', views.customer_page, name="Customerpage"),
     path('courier/', views.courier_page, name="Courierpage"),
