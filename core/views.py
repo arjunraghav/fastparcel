ca1660a74336adcc750b4a3d02fc27ffd12b9e2e
Your Name <you@example.com>
2021-10-02 16:28:10 +0530

    added decorator login required

--- core/views.py
+++ core/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 
+@login_required()
 def customer_page(request):
-    return render(request, 'customer.html')
+    return render(request, 'home.html')
 
 
+@login_required()
 def courier_page(request):
-    return render(request, 'courier.html')
+    return render(request, 'home.html')
     
