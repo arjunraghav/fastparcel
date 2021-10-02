bbe9b526e1303412bc01094fe45167222221285a
Your Name <you@example.com>
2021-10-02 16:58:07 +0530

    added sign up view

--- core/views.py
+++ core/views.py
@@ -3,15 +3,19 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, template_name='home.html')
 
 
 @login_required()
 def customer_page(request):
-    return render(request, 'home.html')
+    return render(request, template_name='home.html')
 
 
 @login_required()
 def courier_page(request):
-    return render(request, 'home.html')
+    return render(request, template_name='home.html')
+
+
+def sign_up(request):
+    return render(request, template_name='sign_up.html')
     
