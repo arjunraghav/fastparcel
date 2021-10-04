6dfd71a89f78629ea2a852eea6d4fd4cf374fffd
Your Name <you@example.com>
2021-10-04 17:11:28 +0530

    Updated forms

--- core/forms.py
+++ core/forms.py
@@ -7,3 +7,14 @@ class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=250)
     first_name = forms.CharField(max_length=150)
     last_name = forms.CharField(max_length=150)
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2',)
+
+    
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        if User.objects.filter(email=email):
+            raise ValidationError("This email exists")
+        return email
